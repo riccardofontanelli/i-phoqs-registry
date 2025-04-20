e6a0ffd61d3dd16b38fc7f39042ac64235deb5aa
riccardofontanelli <riccardo.fontanelli@gmail.com>
2025-04-20 09:48:44 +0200

    Aggiunta DELETE /strumenti/{id}

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 # Permette al frontend React (localhost:3000) di accedere
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",  # per sviluppo locale
+    "https://registry-frontend.onrender.com"  # per deploy su Render
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -30,6 +33,14 @@ def get_strumenti():
 def add_strumento(payload: dict):
     try:
         response = requests.post(f"{DATA_STORE_URL}/strumenti", json=payload)
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.delete("/strumenti/{id}")
+def delete_strumento(id: int):
+    try:
+        response = requests.delete(f"{DATA_STORE_URL}/strumenti/{id}")
         return response.json()
     except Exception as e:
         return {"error": str(e)}
